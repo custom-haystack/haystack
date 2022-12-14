a5c9cbb16aaeb14257cc898e4965dbf2d3f64fd1
Olivia Amici <oliviamga2@yahoo.com>
2022-12-14 18:51:45 -0500

    polished up web app

--- ui/ui/webapp.py
+++ ui/ui/webapp.py
@@ -8,7 +8,7 @@ import pandas as pd
 import streamlit as st
 from annotated_text import annotation
 from markdown import markdown
-
+from PIL import Image
 from ui.utils import (
     haystack_is_ready, 
     query,
@@ -24,7 +24,14 @@ def set_state_if_absent(key, value):
 
 def main():
 
-    st.set_page_config(page_title="GURU AI demo", page_icon="https://haystack.deepset.ai/img/HaystackIcon.png")
+    path = os.path.dirname(__file__)
+
+    favicon_pth = path+'/s2s.png'
+    favicon_im = Image.open(favicon_pth)
+    logo_pth = path+'/s2s_white.png'
+    logo_im = Image.open(logo_pth)
+
+    st.set_page_config(page_title="GURU AI", page_icon=favicon_im) # https://haystack.deepset.ai/img/HaystackIcon.png
     
     set_state_if_absent("question", None)
     set_state_if_absent("answer", None)
@@ -37,16 +44,24 @@ def main():
         st.session_state.results = None
         st.session_state.raw_json = None
 
-    # VW4VROUnMdKaTODi9u
-    st.markdown("![Alt Text](https://media.giphy.com/media/VW4VROUnMdKaTODi9u/giphy.gif)")
+    st.markdown(""" <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style> """, unsafe_allow_html=True)
 
-    st.title("Questions & Answers with GURU AI")
+    st.image(logo_im)
+    
+    st.title("Meet GURU AI.")
+
+    _left, mid, _right = st.columns(3)
+    with mid:
+        st.image('https://media.giphy.com/media/VW4VROUnMdKaTODi9u/giphy.gif')
 
     st.markdown(
             """
-    Based on the FAR pdf booklet, this demo will answer any questions you have about
+    Based on the FAR and DFAR pdf booklets, GURU will answer any questions you have about
 
-    <h3 style='text-align:center;padding: 0 0 1rem;'> Federal Regulation Acquisition</h3>
+    <h3 style='text-align:center;padding: 0 0 1rem;'>Federal Regulation Acquisition</h3>
    
     Ask any question and see if the AI can find the correct answer!
 
@@ -55,23 +70,27 @@ def main():
             unsafe_allow_html=True,
         )
 
+    # st.image('https://media.giphy.com/media/VW4VROUnMdKaTODi9u/giphy.gif')
+    # VW4VROUnMdKaTODi9u
+    # st.markdown("![Alt Text](https://media.giphy.com/media/VW4VROUnMdKaTODi9u/giphy.gif)")
+
     top_k_reader = 1
     top_k_retriever = 10
-    st.sidebar.write("## File Upload:")
-    data_files = st.sidebar.file_uploader("", type=["pdf"], accept_multiple_files=True)
-    for data_file in data_files:
-        # Upload file
-        if data_file:
-            raw_json = upload_doc(data_file)
-            st.sidebar.write(str(data_file.name) + " &nbsp;&nbsp; ✅ ")
-            st.subheader("REST API JSON response")
-            st.sidebar.write(raw_json)
+    # st.sidebar.write("## File Upload:")
+    # data_files = st.sidebar.file_uploader("", type=["pdf"], accept_multiple_files=True)
+    # for data_file in data_files:
+    #     # Upload file
+    #     if data_file:
+    #         raw_json = upload_doc(data_file)
+    #         st.sidebar.write(str(data_file.name) + " &nbsp;&nbsp; ✅ ")
+    #         st.subheader("REST API JSON response")
+    #         st.sidebar.write(raw_json)
 
-    hs_version = ""
-    try:
-        hs_version = f" <small>(v{haystack_version()})</small>"
-    except Exception:
-        pass
+    # hs_version = ""
+    # try:
+    #     hs_version = f" <small>(v{haystack_version()})</small>"
+    # except Exception:
+    #     pass
 
     # Search bar
     question = st.text_input("", value="", max_chars=100, on_change=reset_results)
